bcdb28002e9bcafdac2f50592a5ff96368ed9c4c
calvinswinnen <calvin.swinnen@xt-i.com>
2020-12-20 23:23:47 +0100

    master - add catalogue for constants

--- Java/src/main/java/com/gildedrose/Catalogue.java
+++ Java/src/main/java/com/gildedrose/Catalogue.java
@@ -2,4 +2,6 @@ package com.gildedrose;
 
 public class Catalogue {
     public static final String BRIE = "Aged Brie";
+    public static final String SULFARAS = "Sulfuras, Hand of Ragnaros";
+    public static final String BACKSTAGE_PASS = "Backstage passes to a TAFKAL80ETC concert";
 }
