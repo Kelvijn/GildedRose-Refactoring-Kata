6a3567f36a80072c8ffc88e037f1c97b8ab15a46
calvinswinnen <calvin.swinnen@xt-i.com>
2020-12-20 22:16:03 +0100

    master - init given when then

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -16,60 +16,100 @@ class GildedRoseTest {
 
     @Test
     void degradeQualityWhenDayHasPassed() {
+        // Given
 
+        // When
+
+        // Then
     }
 
     @Test
     void degradeQualityTwiceAsFastWhenSellDateHasPassed() {
+        // Given
 
+        // When
+
+        // Then
     }
 
     @Test
     void qualityOfItemCannotBeNegative() {
+        // Given
 
+        // When
+
+        // Then
     }
 
     @Test
     void agedBrieIncreasesInValueWhenDayHasPassed() {
+        // Given
 
+        // When
+
+        // Then
     }
 
     @Test
     void qualityOfItemCannotBeMoreThan50() {
+        // Given
 
+        // When
+
+        // Then
     }
 
     @Test
     void sulfarasDoesNotDecreaseInQuality() {
+        // Given
 
+        // When
+
+        // Then
     }
 
     @Test
     void sulfarasQualityCannotBeChanged() {
+        // Given
 
+        // When
+
+        // Then
     }
 
     @Test
     void qualityOfBrieImprovesWhenSellInValueApproaches() {
+        // Given
 
+        // When
+
+        // Then
     }
 
     @Test
     void qualityOfBrieIncreasesByTwoWhenSellInValueIsTenOrLess() {
+        // Given
 
+        // When
+
+        // Then
     }
 
     @Test
     void qualityOfBrieDropsToZeroWhenExpired() {
+        // Given
 
+        // When
+
+        // Then
     }
 
     @Test
     void conjuredItemsDegradeInQualityTwiceAsFast() {
+        // Given
 
+        // When
+
+        // Then
     }
-
-
-
-
 }
