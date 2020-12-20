1fba8f6718209718e61bb193382d37d7df9b7696
calvinswinnen <calvin.swinnen@xt-i.com>
2020-12-20 23:13:25 +0100

    master - impl of agedBrieIncreasesInValueWhenDayHasPassed

---
+++ Java/src/main/java/com/gildedrose/Catalogue.java
@@ -0,0 +1,5 @@
+package com.gildedrose;
+
+public class Catalogue {
+    public static final String BRIE = "Aged Brie";
+}
--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -2,6 +2,7 @@ package com.gildedrose;
 
 import org.junit.jupiter.api.Test;
 
+import static com.gildedrose.Catalogue.BRIE;
 import static org.assertj.core.api.Assertions.assertThat;
 
 
@@ -21,39 +22,68 @@ class GildedRoseTest {
     }
 
     @Test
-    void degradeQualityWhenDayHasPassed() {
+    void passageOfDayCountsHowManyDaysItemIsExpired() {
         // Given
+        Item[] items = new Item[]{new Item("item", 0, 50)};
+        GildedRose gildedRose = new GildedRose(items);
 
         // When
+        gildedRose.updateQuality();
 
         // Then
+        assertThat(gildedRose.getItems().get(0).sellIn).isEqualTo(-1);
+    }
+
+    @Test
+    void degradeQualityWhenDayHasPassed() {
+        // Given
+        Item[] items = new Item[]{new Item("item", 10, 50)};
+        GildedRose gildedRose = new GildedRose(items);
+
+        // When
+        gildedRose.updateQuality();
+
+        // Then
+        assertThat(gildedRose.getItems().get(0).quality).isEqualTo(49);
     }
 
     @Test
     void degradeQualityTwiceAsFastWhenSellDateHasPassed() {
         // Given
+        Item[] items = new Item[]{new Item("item", 0, 50)};
+        GildedRose gildedRose = new GildedRose(items);
 
         // When
+        gildedRose.updateQuality();
 
         // Then
+        assertThat(gildedRose.getItems().get(0).quality).isEqualTo(48);
     }
 
     @Test
     void qualityOfItemCannotBeNegative() {
         // Given
+        Item[] items = new Item[]{new Item("item", 0, 0)};
+        GildedRose gildedRose = new GildedRose(items);
 
         // When
+        gildedRose.updateQuality();
 
         // Then
+        assertThat(gildedRose.getItems().get(0).quality).isEqualTo(0);
     }
 
     @Test
     void agedBrieIncreasesInValueWhenDayHasPassed() {
         // Given
+        Item[] items = new Item[]{new Item(BRIE, 20, 30)};
+        GildedRose gildedRose = new GildedRose(items);
 
         // When
+        gildedRose.updateQuality();
 
         // Then
+        assertThat(gildedRose.getItems().get(0).quality).isEqualTo(31);
     }
 
     @Test
