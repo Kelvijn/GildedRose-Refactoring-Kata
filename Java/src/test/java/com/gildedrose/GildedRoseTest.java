8b87660af9d60875257c4b5a9a3b723d1263fbe1
calvinswinnen <calvin.swinnen@xt-i.com>
2020-12-20 23:24:20 +0100

    master - add test cases for existing updateQuality() method

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -2,7 +2,7 @@ package com.gildedrose;
 
 import org.junit.jupiter.api.Test;
 
-import static com.gildedrose.Catalogue.BRIE;
+import static com.gildedrose.Catalogue.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 
@@ -89,63 +89,78 @@ class GildedRoseTest {
     @Test
     void qualityOfItemCannotBeMoreThan50() {
         // Given
+        Item[] items = new Item[]{new Item(BRIE, 5, 50)};
+        GildedRose gildedRose = new GildedRose(items);
 
         // When
+        gildedRose.updateQuality();
 
         // Then
+        assertThat(gildedRose.getItems().get(0).quality).isEqualTo(50);
     }
 
     @Test
     void sulfarasDoesNotDecreaseInQuality() {
         // Given
+        Item[] items = new Item[]{new Item(SULFARAS, 0, 80)};
+        GildedRose gildedRose = new GildedRose(items);
 
         // When
+        gildedRose.updateQuality();
 
         // Then
+        assertThat(gildedRose.getItems().get(0).quality).isEqualTo(80);
     }
 
     @Test
-    void sulfarasQualityCannotBeChanged() {
+    void qualityOfBackStagePassIncreasesByOne() {
         // Given
+        Item[] items = new Item[]{new Item(BACKSTAGE_PASS, 20, 30)};
+        GildedRose gildedRose = new GildedRose(items);
 
         // When
+        gildedRose.updateQuality();
 
         // Then
+        assertThat(gildedRose.getItems().get(0).quality).isEqualTo(31);
     }
 
     @Test
-    void qualityOfBrieImprovesWhenSellInValueApproaches() {
+    void qualityOfBackStagePassIncreasesByTwoWhenSellInIsTenOrLess() {
         // Given
+        Item[] items = new Item[]{new Item(BACKSTAGE_PASS, 10, 30)};
+        GildedRose gildedRose = new GildedRose(items);
 
         // When
+        gildedRose.updateQuality();
 
         // Then
+        assertThat(gildedRose.getItems().get(0).quality).isEqualTo(32);
     }
 
     @Test
-    void qualityOfBrieIncreasesByTwoWhenSellInValueIsTenOrLess() {
+    void qualityOfBackStagePassIncreasesByThreeWhenSellInIsFiveOrLess() {
         // Given
+        Item[] items = new Item[]{new Item(BACKSTAGE_PASS, 5, 30)};
+        GildedRose gildedRose = new GildedRose(items);
 
         // When
+        gildedRose.updateQuality();
 
         // Then
+        assertThat(gildedRose.getItems().get(0).quality).isEqualTo(33);
     }
 
     @Test
-    void qualityOfBrieDropsToZeroWhenExpired() {
+    void qualityOfBackStagePassDropsToZeroWhenConcertHasPassed() {
         // Given
+        Item[] items = new Item[]{new Item(BACKSTAGE_PASS, 0, 30)};
+        GildedRose gildedRose = new GildedRose(items);
 
         // When
+        gildedRose.updateQuality();
 
         // Then
-    }
-
-    @Test
-    void conjuredItemsDegradeInQualityTwiceAsFast() {
-        // Given
-
-        // When
-
-        // Then
+        assertThat(gildedRose.getItems().get(0).quality).isEqualTo(0);
     }
 }
