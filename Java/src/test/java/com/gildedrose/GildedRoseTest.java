78c68ff68dc9fcb04d26922f46564a20620e038c
calvinswinnen <calvin.swinnen@xt-i.com>
2020-12-20 22:13:22 +0100

    master - init test cases

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -8,10 +8,68 @@ class GildedRoseTest {
 
     @Test
     void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        Item[] items = new Item[]{new Item("foo", 0, 0)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals("fixme", app.items[0].name);
     }
+
+    @Test
+    void degradeQualityWhenDayHasPassed() {
+
+    }
+
+    @Test
+    void degradeQualityTwiceAsFastWhenSellDateHasPassed() {
+
+    }
+
+    @Test
+    void qualityOfItemCannotBeNegative() {
+
+    }
+
+    @Test
+    void agedBrieIncreasesInValueWhenDayHasPassed() {
+
+    }
+
+    @Test
+    void qualityOfItemCannotBeMoreThan50() {
+
+    }
+
+    @Test
+    void sulfarasDoesNotDecreaseInQuality() {
+
+    }
+
+    @Test
+    void sulfarasQualityCannotBeChanged() {
+
+    }
+
+    @Test
+    void qualityOfBrieImprovesWhenSellInValueApproaches() {
+
+    }
+
+    @Test
+    void qualityOfBrieIncreasesByTwoWhenSellInValueIsTenOrLess() {
+
+    }
+
+    @Test
+    void qualityOfBrieDropsToZeroWhenExpired() {
+
+    }
+
+    @Test
+    void conjuredItemsDegradeInQualityTwiceAsFast() {
+
+    }
+
+
+
 
 }
