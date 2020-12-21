5c2ff922c34826a0c5c136f0391a2b1aed6ccae7
calvinswinnen <calvin.swinnen@xt-i.com>
2020-12-21 18:20:33 +0100

    master - simplify test assertion

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -191,7 +191,7 @@ class GildedRoseTest {
         gildedRose.updateQuality();
 
         // Then
-        assertThat(gildedRose.getItems().get(0).quality).isEqualTo(0);
+        assertThat(gildedRose.getItems().get(0).quality).isZero();
     }
 
     @Test
