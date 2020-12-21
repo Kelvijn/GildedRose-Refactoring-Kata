2352bf6dce44ea27103e1fc8e4ee17e85b505805
calvinswinnen <calvin.swinnen@xt-i.com>
2020-12-21 13:52:50 +0100

    master - remove redundant check

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,10 +1,7 @@
 package com.gildedrose;
 
-import com.gildedrose.exceptions.ItemValidationException;
-
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 class GildedRose {
     Item[] items;
@@ -22,17 +19,6 @@ class GildedRose {
     }
 
     public void updateQuality() {
-        List<String> catalogue = Arrays.stream(Catalogue.values()).map(Catalogue::getName).collect(Collectors.toList());
-
-        boolean itemsAreAddedToCatalogue = getItems()
-                .stream()
-                .map(item -> item.name)
-                .allMatch(catalogue::contains);
-
-        if (!itemsAreAddedToCatalogue) {
-            throw new ItemValidationException("Please add all items to catalogue");
-        }
-
         setItems(getItems()
                 .stream()
                 .map(item -> Catalogue.withName(item.name).process(item))
