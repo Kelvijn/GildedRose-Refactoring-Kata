aa9f7ffd9a21665daa36aa6f399791cc386faede
calvinswinnen <calvin.swinnen@xt-i.com>
2020-12-21 18:17:16 +0100

    master - fix sonarlint issue

--- Java/src/main/java/com/gildedrose/Configuration.java
+++ Java/src/main/java/com/gildedrose/Configuration.java
@@ -3,4 +3,8 @@ package com.gildedrose;
 public class Configuration {
     public static final int MAX_QUALITY = 50;
     public static final int SULFARAS_QUALITY = 80;
+
+    private Configuration() {
+        throw new IllegalStateException("Utility class");
+    }
 }
