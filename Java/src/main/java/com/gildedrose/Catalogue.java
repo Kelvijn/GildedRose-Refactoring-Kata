60de9c1c9b5e86f4422bc8546f7a513f03d3377a
calvinswinnen <calvin.swinnen@xt-i.com>
2020-12-21 14:16:38 +0100

    master - refactoring

--- Java/src/main/java/com/gildedrose/Catalogue.java
+++ Java/src/main/java/com/gildedrose/Catalogue.java
@@ -35,15 +35,11 @@ public enum Catalogue implements Processable {
             item.sellIn = --item.sellIn;
             if (!isMaxQuality(item.quality)) {
                 item.quality = item.quality + 1;
-                if (item.sellIn < 11) {
-                    if (!isMaxQuality(item.quality)) {
-                        item.quality = item.quality + 1;
-                    }
+                if (item.sellIn < 10 && !isMaxQuality(item.quality)) {
+                    item.quality = item.quality + 1;
                 }
-                if (item.sellIn < 6) {
-                    if (!isMaxQuality(item.quality)) {
-                        item.quality = item.quality + 1;
-                    }
+                if (item.sellIn < 5 && !isMaxQuality(item.quality)) {
+                    item.quality = item.quality + 1;
                 }
             }
             if (item.sellIn <= 0) {
@@ -55,7 +51,7 @@ public enum Catalogue implements Processable {
     CONJURED("Conjured Mana Cake") {
         @Override
         public Item process(Item item) {
-            item.sellIn = item.sellIn--;
+            item.sellIn = --item.sellIn;
             if (item.quality >= 0) {
                 item.quality = item.quality - 1;
                 if (item.quality >= 0) {
--- Java/src/main/java/com/gildedrose/Configuration.java
+++ Java/src/main/java/com/gildedrose/Configuration.java
@@ -2,5 +2,6 @@ package com.gildedrose;
 
 public class Configuration {
     public static final int MAX_QUALITY = 50;
+    public static final int DEFAULT_DEGRADATION = 1;
     public static final int SULFARAS_QUALITY = 80;
 }
